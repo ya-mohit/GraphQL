f481f2f886868ee665fb7be82ad7e28fd79f337f
Mohit Yadav <mohityadav@Mohits-MacBook-Air.local>
2026-05-03 17:19:29 +0530

    Caching in GraphQL

--- src/main/java/com/mohit/graohql/GraphQL/Controller/MovieGraphController.java
+++ src/main/java/com/mohit/graohql/GraphQL/Controller/MovieGraphController.java
@@ -31,15 +31,12 @@ public class MovieGraphController {
     public Movie addReviewToMovie(@Argument String name, @Argument Review review) {
         Movie movie = movieService.getAMovieByName(name);
         if (movie != null) {
-            // Initialize reviews list if null
             if (movie.getReviews() == null) {
                 movie.setReviews(new java.util.ArrayList<>());
             }
             // Set the bidirectional relationship
             review.setMovie(movie);
-            // Add review to the movie
             movie.getReviews().add(review);
-            // Save the updated movie with the new review
             movieService.saveMovie(movie);
             return movie;
         }
--- src/main/java/com/mohit/graohql/GraphQL/Service/MovieService.java
+++ src/main/java/com/mohit/graohql/GraphQL/Service/MovieService.java
@@ -3,6 +3,8 @@ package com.mohit.graohql.GraphQL.Service;
 import com.mohit.graohql.GraphQL.Entity.Movie;
 import com.mohit.graohql.GraphQL.Repository.MovieRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,22 +15,27 @@ public class MovieService {
     @Autowired
     private MovieRepo movieRepo;
 
+    @Cacheable(value = "movie", key = "#id")
     public Movie getAMovieById(int id) {
         return movieRepo.findById(id).orElse(null);
     }
 
+    @Cacheable(value = "movieByName", key = "#name")
     public Movie getAMovieByName(String name) {
         return movieRepo.findByName(name);
     }
 
+    @Cacheable(value = "allMovies")
     public List<Movie> getAllMovies() {
         return movieRepo.findAll();
     }
 
+    @Cacheable(value = "moviesByCategory", key = "#category")
     public List<Movie> getMoviesByCategory(String category) {
         return movieRepo.findByCategory(category);
     }
 
+    @CacheEvict(value = {"allMovies", "moviesByCategory", "movieByName"}, allEntries = true)
     public Movie saveMovie(Movie movie) {
         return movieRepo.save(movie);
     }
